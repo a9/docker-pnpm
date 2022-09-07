70c9819ea3d062994fda02b3c51eb27e4f2f36ec
kuqoi <kuqoi@qq.com>
2022-09-07 12:25:50 +0800

    7.11.0

--- env.sh
+++ env.sh
@@ -3,8 +3,8 @@
 export NAME=kuqoi/pnpm
 export VERSION=alpine
 export VERSION_MAJAR=7-alpine
-export VERSION_MINOR=7.9-alpine
-export VERSION_PATCH=7.9.5-alpine
+export VERSION_MINOR=7.11-alpine
+export VERSION_PATCH=7.11.0-alpine
 
 export TAG="${NAME}:${VERSION}"
 export TAG_MAJOR="${NAME}:${VERSION_MAJAR}"
