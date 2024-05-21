73a0b60e7c3727c244f976de7241fb64a500c849
a9 <a9@users.noreply.github.com>
2024-05-21 19:35:25 +0800

    9.1.2

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ export NAME=kuqoi/pnpm
 export VERSION=alpine
 export VERSION_MAJAR=9-alpine
 export VERSION_MINOR=9.1-alpine
-export VERSION_PATCH=9.1.1-alpine
+export VERSION_PATCH=9.1.2-alpine
 
 export TAG="${NAME}:${VERSION}"
 export TAG_MAJOR="${NAME}:${VERSION_MAJAR}"
